1d28afe0ae9468096d140ab9dc6e76bb4e9f2922
itsprinsprays <prncbntz@gmail.com>
2026-06-22 23:07:52 +0800

    JwtUtil : Generate Token Function added

--- src/main/java/security/JwtUtil.java
+++ src/main/java/security/JwtUtil.java
@@ -1,5 +1,47 @@
 package security;
 
+
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component("util")
 public class JwtUtil {
+	
+	@Value("${jwt.secret}")
+	public String secret;
+	
+	@Value("${jwt.accessExpiration}")
+	public long accessExpiration;
+	
+	@Value("${jwt.refreshExpiration}")
+	public long refreshExpiration;
+	
+	public SecretKey SECRET;
+	
+	public void init() {
+		this.SECRET = Keys.hmacShaKeyFor(secret.getBytes());
+	}
+	
+	public String generateToken(String username) {
+		
+		return Jwts.builder()
+				.claim(Claims.SUBJECT, username)
+				.claim("type", "access")
+				.claim(Claims.ISSUED_AT, new Date())
+				.claim(Claims.EXPIRATION, new Date(
+						System.currentTimeMillis() + accessExpiration))
+				.signWith(SECRET)
+				.compact();
+		
+	}
 
 }
