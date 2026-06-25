728654a88f99bca8da8cc87feba4d0f851846a40
itsprinsprays <prncbntz@gmail.com>
2026-06-25 17:06:21 +0800

    SecurityFilterChainConfig added

---
+++ src/main/java/security1/SecurityFilterChainConfig.java
@@ -0,0 +1,41 @@
+package security1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilterChainConfig {
+	
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	public SecurityFilterChainConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+	
+	@Bean
+	public SecurityFilterChain filter(HttpSecurity http) throws Exception{
+		
+		return http
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth
+						
+						.requestMatchers(HttpMethod.GET, "/api/").permitAll()
+						
+						.requestMatchers(HttpMethod.POST, "/api/").hasRole("ADMIN")
+						
+						.requestMatchers(HttpMethod.PATCH, "/api/").hasAnyRole("ADMIN", "HR")
+						
+						.anyRequest().authenticated()
+						)
+				
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.build();
+	}
+
+}
